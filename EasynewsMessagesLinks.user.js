d9ccefe53894a09e2049705f8da192bb9ae3602c
Bryan Creel <scytalezero@users.noreply.github.com>
2014-12-04 10:25:56 -0500

    Update EasynewsMessagesLinks.user.js

--- EasynewsMessagesLinks.user.js
+++ EasynewsMessagesLinks.user.js
@@ -4,10 +4,11 @@
 // @description Re-writes the links to Global5.
 // @author      Bryan Creel
 // @homepageURL https://github.com/scytalezero/EasynewsMessagesLinks
-// @updateURL   
+// @updateURL   https://github.com/scytalezero/EasynewsMessagesLinks/raw/master/EasynewsMessagesLinks.user.js
+// @icon        https://pbs.twimg.com/profile_images/479627852757733376/8v9zH7Yo_normal.jpeg
 // @include     https://*.easynews.com/edit/messages.phtml*
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js
-// @version     1.0
+// @version     1.1
 // ==/UserScript==
 
 $link = $("pre > a");
