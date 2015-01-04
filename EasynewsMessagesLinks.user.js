4e9ef71aef56c56b10ad8fc7560bee4f7a66c700
Bryan Creel <scytalezero@users.noreply.github.com>
2015-01-04 12:05:18 -0500

    Update EasynewsMessagesLinks.user.js

--- EasynewsMessagesLinks.user.js
+++ EasynewsMessagesLinks.user.js
@@ -3,14 +3,14 @@
 // @namespace   ligature.me
 // @description Re-writes the links to Global5.
 // @author      Bryan Creel
+// @grant       none
 // @homepageURL https://github.com/scytalezero/EasynewsMessagesLinks
 // @updateURL   https://github.com/scytalezero/EasynewsMessagesLinks/raw/master/EasynewsMessagesLinks.user.js
 // @downloadURL https://github.com/scytalezero/EasynewsMessagesLinks/raw/master/EasynewsMessagesLinks.user.js
 // @icon        https://pbs.twimg.com/profile_images/479627852757733376/8v9zH7Yo_normal.jpeg
 // @include     https://*.easynews.com/edit/messages.phtml*
 // @include     https://account.easynews.com/messages.php*
-// @require     https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js
-// @version     1.2
+// @version     1.3
 // ==/UserScript==
 
 $link = $("pre > a");
